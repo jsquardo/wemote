db164e2ac7e6d8d1ce258562dea5876e936238a7
Johnny Squardo <squ@rdo.host>
2020-04-02 21:51:15 -0400

    update

--- src/components/JobBox.vue
+++ src/components/JobBox.vue
@@ -5,7 +5,7 @@
         <div class="box mt-2 mb-2">
           <div class="box-flex">
             <div class="left">
-              <img :src="job.company_logo" alt />
+              <span class="company_logo" v-bind:style="{backgroundImage: url(job.company_logo)}"></span>
               <p>{{ job.company }}</p>
             </div>
             <div class="right"></div>
@@ -47,5 +47,15 @@ export default {
 .right {
   display: flex;
   width: 50%;
+}
+
+.company_logo {
+  border-radius: 100%;
+  height: 4rem;
+  width: 4rem;
+  background: center no-repeat;
+  background-image: none;
+  background-size: auto;
+  background-size: cover;
 }
 </style>
